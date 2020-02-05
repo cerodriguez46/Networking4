37620525dbfee933b44454ebd73faf37fcfcb6ef
Christopher Rodriguez <christophererodriguez70@gmail.com>
2020-02-05 12:02:24 -0600

    Initial commit

--- app/src/main/java/com/example/networking4/MainActivity.java
+++ app/src/main/java/com/example/networking4/MainActivity.java
@@ -17,6 +17,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
@@ -48,81 +49,84 @@ layoutManager = new LinearLayoutManager(this);
 rvView.setLayoutManager(layoutManager);
 
 
-
-        new fetchPosts(rvView).execute(url);
+        new fetchPosts(postList).execute(url);
 
     }
+//first param is type that is passed from .execute(), which also gets passed as param to doInBackground
+    //second param is for onProgressUpdate
+    //third param is return type of doInBackground which gets returned and then passed as param in onPostExecute
 
-private class fetchPosts extends AsyncTask<String, Void, String>{
+    //first param needs to be string to pass the url
+    //third param needs to be the arraylist so that can be returned after the background parsing is done and then set to recyclerview
+    private class fetchPosts extends AsyncTask<String, Void, ArrayList<PostModel>> {
 
+        ArrayList<PostModel> returnedArrayList;
 
-RecyclerView rv1;
-
-
-    public fetchPosts(RecyclerView rv) {
-this.rv1 = rv;
-    }
-
-
-
-    @Override
-    protected String doInBackground(String... strings) {
-
-        String id= "Unknown";
-
-        try {
-            URL url = new URL(strings[0]);
-            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-
-            InputStream stream = new BufferedInputStream(httpURLConnection.getInputStream());
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stream));
-            StringBuilder builder = new StringBuilder();
-
-            String inputString;
-            while ((inputString = bufferedReader.readLine())!= null) {
-                builder.append(inputString);
-            }
-
-            //JSON arrays always have to be passed to lists or recyclerviews
-            JSONObject topLevel = new JSONObject(builder.toString());
-
-            JSONArray data = topLevel.getJSONArray("data");
-
-           for (int i = 0; i <data.length(); i++) {
-                JSONObject c = data.getJSONObject(i);
-               id = c.getString("id");
-String urlJson = c.getString("url");
-String comments = c.getString("comments_url");
-String title = c.getString("title");
-String slug = c.getString("slug");
-String image = c.getString("featured_image");
-String body = c.getString("body");
-String created = c.getString("created_at");
-String updated = c.getString("updated_at");
-
-PostModel postModel = new PostModel(id, urlJson, comments, title, slug, image, body, created, updated);
-postList.add(postModel);
-
-
-           }
-
-        } catch(IOException | JSONException e) {
-            e.printStackTrace();
+        public fetchPosts(ArrayList<PostModel> returnedArrayList) {
+            this.returnedArrayList = returnedArrayList;
         }
 
+        @Override
+        protected ArrayList<PostModel> doInBackground(String... strings) {
+            try {
+                URL url = new URL(strings[0]);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
 
-        return id;
-    }
+                InputStream stream = new BufferedInputStream(httpURLConnection.getInputStream());
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stream));
+                StringBuilder builder = new StringBuilder();
 
-    @Override
-    protected void onPostExecute(String s) {
+                String inputString;
+                while ((inputString = bufferedReader.readLine()) != null) {
+                    builder.append(inputString);
+                }
+
+                //JSON arrays always have to be passed to lists or recyclerviews
+                JSONObject topLevel = new JSONObject(builder.toString());
+
+                JSONArray data = topLevel.getJSONArray("data");
+
+                for (int i = 0; i < data.length(); i++) {
+                    JSONObject c = data.getJSONObject(i);
+                    String id = c.getString("id");
+                    String urlJson = c.getString("url");
+                    String comments = c.getString("comments_url");
+                    String title = c.getString("title");
+                    String slug = c.getString("slug");
+                    String image = c.getString("featured_image");
+                    String body = c.getString("body");
+                    String created = c.getString("created_at");
+                    String updated = c.getString("updated_at");
+
+                    PostModel postModel = new PostModel(id, urlJson, comments, title, slug, image, body, created, updated);
+                    returnedArrayList.add(postModel);
 
 
+                }
 
-mAdapter = new PostAdapter(MainActivity.this, postList);
-rvView.setAdapter(mAdapter);
-mAdapter.notifyDataSetChanged();
+            } catch (IOException | JSONException e) {
+                e.printStackTrace();
+            }
 
+
+            return returnedArrayList;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<PostModel> postModels) {
+
+            mAdapter = new PostAdapter(MainActivity.this, postModels);
+            rvView.setAdapter(mAdapter);
+
+            mAdapter.notifyDataSetChanged();
+        }
     }
 }
-}
+
+
+
+
+
+
+
+
--- app/src/main/java/com/example/networking4/PostAdapter.java
+++ app/src/main/java/com/example/networking4/PostAdapter.java
@@ -33,6 +33,14 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
     public void onBindViewHolder(ViewHolder holder, int position) {
 PostModel postModel = postModelArrayList.get(position);
 holder.tv1.setText(postModel.getId());
+        holder.tv2.setText(postModel.getUrl());
+        holder.tv3.setText(postModel.getComments());
+        holder.tv4.setText(postModel.getTitle());
+        holder.tv5.setText(postModel.getSlug());
+        holder.tv6.setText(postModel.getImage());
+        holder.tv7.setText(postModel.getBody());
+        holder.tv8.setText(postModel.getCreated());
+        holder.tv9.setText(postModel.getUpdated());
     }
 
     @Override
@@ -56,13 +64,13 @@ holder.tv1.setText(postModel.getId());
             super(itemView);
             tv1 = itemView.findViewById(R.id.textView);
             tv2 = itemView.findViewById(R.id.textView2);
-            tv3 = itemView.findViewById(R.id.textView);
-            tv4 = itemView.findViewById(R.id.textView);
-            tv5 = itemView.findViewById(R.id.textView);
-            tv6 = itemView.findViewById(R.id.textView);
-            tv7 = itemView.findViewById(R.id.textView);
-            tv8 = itemView.findViewById(R.id.textView);
-            tv9 = itemView.findViewById(R.id.textView);
+            tv3 = itemView.findViewById(R.id.textView3);
+            tv4 = itemView.findViewById(R.id.textView4);
+            tv5 = itemView.findViewById(R.id.textView5);
+            tv6 = itemView.findViewById(R.id.textView6);
+            tv7 = itemView.findViewById(R.id.textView7);
+            tv8 = itemView.findViewById(R.id.textView8);
+            tv9 = itemView.findViewById(R.id.textView9);
         }
     }
 }
